3e67400820e77147d4fe94a2232ccbd01b48aa4c
PhilipRobertsson <phiro138@student.liu.se>
2025-09-04 19:48:56 +0200

    Added glm to include list

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "include/glad/glad.h"
 #include "include/GLFW/glfw3.h"
+#include "glm.hpp"
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
