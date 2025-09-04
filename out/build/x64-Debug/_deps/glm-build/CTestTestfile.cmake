bc9c5df5afc40e7e29bea3c45d130d66e50f1bfb
PhilipRobertsson <phiro138@student.liu.se>
2025-09-04 19:31:08 +0200

    Initial push

--- out/build/x64-Debug/_deps/glm-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/almal/Desktop/Termin9/TNCG15/Project/OurRenderer/out/build/x64-Debug/_deps/glm-src
-# Build directory: C:/Users/almal/Desktop/Termin9/TNCG15/Project/OurRenderer/out/build/x64-Debug/_deps/glm-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
