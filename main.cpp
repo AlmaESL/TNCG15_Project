4ef84aa9e3557744b5afd21a61ea2e2590af2e51
PhilipRobertsson <phiro138@student.liu.se>
2025-10-06 13:05:53 +0200

    Added timekeeping to main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 
 #include "include/roomClass.h"
 #include "include/camera.h"
@@ -8,12 +9,27 @@ int main() {
 	int WIDTH = 1024;
 	int HEIGHT = 1024; 
 
+	using std::chrono::high_resolution_clock;
+	using std::chrono::duration_cast;
+	using std::chrono::duration;
+	using std::chrono::milliseconds;
 
 	Camera cam; 
 	Scene scene;
 	
 	Renderer renderer;
+	auto t1 = high_resolution_clock::now();
 	renderer.render(scene, cam, WIDTH, HEIGHT, "test.ppm");
+	auto t2 = high_resolution_clock::now();
+
+	/* Getting number of milliseconds as an integer. */
+	auto ms_int = duration_cast<milliseconds>(t2 - t1);
+
+	/* Getting number of milliseconds as a double. */
+	duration<double, std::milli> ms_double = t2 - t1;
+
+	std::cout <<"Rendering time: " << ms_int.count() << "ms\n";
+
 	std::cout << "Rendered to test.ppm\n";
 	return 0;
 
